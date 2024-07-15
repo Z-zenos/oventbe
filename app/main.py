08ca5fc6f78bcbf48fda23e0bb5187d219934038
Z-zenos <hoanganhtuanbk2001@gmail.com>
2024-07-16 02:36:36 +0700

    Backend setup

--- app/main.py
+++ app/main.py
@@ -10,21 +10,20 @@ app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
+    name: str = ""
+    is_offer: bool = False
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None) -> dict[str, str | None]:
+#     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item) -> dict[str, int]:
+#     return {"item_name": item.name, "item_id": item_id}
